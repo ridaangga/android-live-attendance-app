36a189f330b847f8171ec6358f6a9d3e2a8e98b7
Rida Angga Kusuma <rida.angga@gmail.com>
2023-01-17 09:35:41 +0700

    ui: Add Forgot Password Page

---
+++ app/src/main/java/com/example/liveattendance/views/forgotpass/ForgotPasswordActivity.kt
@@ -0,0 +1,24 @@
+package com.example.liveattendance.views.forgotpass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.liveattendance.R
+import com.example.liveattendance.databinding.ActivityForgotPasswordBinding
+
+class ForgotPasswordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityForgotPasswordBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+    }
+
+    private fun init() {
+        setSupportActionBar(binding.tbForgotPassword)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
